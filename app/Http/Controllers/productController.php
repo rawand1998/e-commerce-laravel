baa54b3f5e114da539253310533c116b4e95bb22
rawand1998 <rawandgaradh@gmail.com>
2022-03-10 17:43:51 +0200

    cart

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Product;
 use Illuminate\Http\Request;
+use App\Cart;
 
 class productController extends Controller
 {
@@ -14,5 +15,16 @@ class productController extends Controller
         $data = Product::find($id);
         return  view('detail',['product' => $data]);
        
+    }
+    function addToCart(Request $request){
+        if($request->session()->has('user')){
+           $cart = new Cart;
+           $cart->user_id = $request->session()->get('user')['id'];
+           $cart->product_id= $request->product_id;
+           $cart->save();
+           return redirect('/');
+        }else{
+            return redirect('/login');}
+
     }
 }
