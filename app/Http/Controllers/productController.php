437b1e6644d8f182b74fc377c33ee6d105df261c
rawand1998 <rawandgaradh@gmail.com>
2022-03-10 16:58:19 +0200

    product controller and model

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -1,12 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Product;
 use Illuminate\Http\Request;
 
 class productController extends Controller
 {
     function index(){
-        return "ij";
+       $data =Product::all();
+        return  view('products',['products' => $data]);
+    }
+    function detail($id){
+        $data = Product::find($id);
+        return  view('detail',['product' => $data]);
+       
     }
 }
