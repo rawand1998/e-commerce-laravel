91953b598c77b065549e3426d9243476f3dfe6f7
rawand1998 <rawandgaradh@gmail.com>
2022-03-10 16:12:50 +0200

    login controller

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,9 +3,9 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 col-sm-offset-4">
-            <form >
+            <form action="login" method="POST">
                 <div class="form-group">
-                
+                @csrf
                 <label for="exampleInputEmail1">Email address</label>
                 <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\controllers\userController;
+use App\Http\controllers\productController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +21,8 @@ Route::get('/master', function () {
 Route::get('/login', function () {
     return view('login');
 });
+Route::post("/login",[userController::class,'login']);
+
+Route::get("/",[productController::class,'index']);
+
 
