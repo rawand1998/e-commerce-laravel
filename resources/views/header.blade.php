412f5d450dc816fab0943c26c8616cf3831f989c
rawand1998 <rawandgaradh@gmail.com>
2022-03-10 13:44:07 +0200

    edit boostrap cdn

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
@@ -29,12 +20,10 @@
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
       </ul>
-    
+
       <ul class="nav navbar-nav navbar-right">
           <li ><a href="">Cart (0)</a></li>
       </ul>
     </div>
   </div>
 </nav>
-</body>
-</html>
